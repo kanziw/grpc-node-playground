bb2ff2aa64fb74152137a7945bcd34fd3f149bfc
Jiwoong Jung <kanziwoong@gmail.com>
2024-08-14 00:44:51 +0900

    Test grpc client based on connect-es

---
+++ connect-es-client/e2e.ts
@@ -0,0 +1,29 @@
+import { createPromiseClient } from '@connectrpc/connect';
+import { createGrpcTransport } from '@connectrpc/connect-node';
+import { EchoService } from '~/connect-es/__proto__/echo/v1/echo_connect.js';
+import { makeGrpcServer } from '~/connect-es/server.js';
+
+const GRPC_SERVER_PORT = Number(process.env.CONNECT_ES_GRPC_SERVER_PORT) || 8085;
+
+async function main() {
+  const grpcServer = makeGrpcServer();
+  grpcServer.listenAndServe(GRPC_SERVER_PORT);
+
+  const client = createPromiseClient(
+    EchoService,
+    createGrpcTransport({
+      baseUrl: `http://localhost:${GRPC_SERVER_PORT}`,
+      httpVersion: '2',
+    }),
+  );
+
+  const resp = await client.echo({ message: 'Hello, connect-es!' });
+  console.log(resp);
+
+  process.exit(0);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
--- ts-proto-client/e2e.ts
+++ ts-proto-client/e2e.ts
@@ -7,12 +7,13 @@ const GRPC_SERVER_PORT = process.env.TS_PROTO_GRPC_SERVER_PORT ?? '8086';
 async function main() {
   const grpcServer = makeGrpcServer();
   await grpcServer.start(`0.0.0.0:${GRPC_SERVER_PORT}`);
-  console.log(`Start gRPC Server w/ ts-proto on port ${GRPC_SERVER_PORT}`);
 
   const client = makeGrpcClient(EchoServiceClient, `dns:///localhost:${GRPC_SERVER_PORT}`);
 
   const resp = await client.echo({ message: 'Hello, ts-proto!' });
   console.log(resp);
+
+  await grpcServer.close();
   process.exit(0);
 }
 
